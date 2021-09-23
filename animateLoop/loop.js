83ec345a3fd86ddc340c0208650f7ba036dc6c50
Steven Hyatt <45152664+Ragnar18@users.noreply.github.com>
2021-09-23 18:42:43 -0400

    Add files via upload

---
+++ animateLoop/loop.js
@@ -0,0 +1,63 @@
+/**@type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+CANVAS_WIDTH = canvas.width = 512;
+CANVAS_HEIGHT = canvas.height = 448;
+const numberOfEnemies = 10;
+const enemiesArray = [];
+
+let gameFrame = 0;
+
+class Enemy {
+    constructor() {
+        this.image = new Image();
+        this.image.src = 'enemy1.png';
+        
+        //this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 3;
+        this.height = this.spriteHeight / 3;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    }
+    update() {
+        this.x += Math.random() * 2 - 1;
+        this.y += Math.random() * 2 - 1;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+        
+    }
+    draw() {
+        
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, 
+            this.x, this.y, this.width, this.height);
+    }
+};
+
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy());
+}
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    });
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+let firstName = 'steve';
+let lastName = 'hyatt';
+let role = 'developer';
+
+let msg = firstName + " " + lastName + ":" + role;
+console.log(msg);
